2b88b2dd73e5c04166c53364fd9f7d4ccd90bc22
metw0 <metwojustfunny@duck.com>
2026-08-04 19:43:13 +0300

    feat: add CLI logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <string>
+#include <CLI/CLI.hpp>
 
 int main(int argc, char **argv) {
-    std::cout << "hello world!" << std::endl;
+    CLI::App csmt{"csmt - small utility that will help you run your scripts (in bash, python, etc.) more conveniently and quickly"};
+
+    std::string script_to_run, script_to_add, script_to_remove;
+
+    auto *run = csmt.add_subcommand("run", "run your script");
+    run -> add_option("script_to_run", script_to_run, "script you need to run"); 
+    auto *add = csmt.add_subcommand("add", "add your script");
+    add -> add_option("script_to_add", script_to_add, "script you need to add (with extension)");
+    auto *rm = csmt.add_subcommand("rm", "remove your script");
+    rm -> add_option("script_to_remove", script_to_remove, "script you need to remove");
+
+    CLI11_PARSE(csmt, argc, argv);
+
+    if(*run) {
+        std::cout << "you selected 'run' and typed " << script_to_run << "\n";
+    } else if (*add) {
+        std::cout << "you selected 'add' and typed " << script_to_add << "\n";
+    } else if (*rm) {
+        std::cout << "you selected 'rm' and typed " << script_to_remove << "\n";
+    }
+
     return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -4,10 +4,14 @@ set_version("1.0.0")
 set_languages("c++17")
 add_rules("mode.debug", "mode.release")
 
-target("name")
+add_requires("cli11")
+
+target("csmt")
     set_kind("binary")
     add_files("src/*.cpp")
     add_includedirs("include")
+
+    add_packages("cli11")
 
     add_syslinks("pthread", "dl")
     
